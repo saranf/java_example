61a5990a934bf814c26926baf68393bc8a4e33b7
saranf <qorwlsdk1995@naver.com>
2021-08-02 21:37:20 +0900

    Binary Test

---
+++ BinaryTest.java
@@ -0,0 +1,15 @@
+public class BinaryTest{
+    public static void main(String[] args) {
+        //정수를 많이 표현하는 자료형 : int 
+        int num = 10;
+        int bNum = 0B1010;
+        int oNum = 012;
+        int xNum = 0xA;
+
+        System.out.println(num); //10진수 
+        System.out.println(bNum); //바이너리
+        System.out.println(oNum); //8진수
+        System.out.println(xNum); //16진수
+
+    }
+}
