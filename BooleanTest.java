d4edf03223398199b07f1785ddd4cbaaccad2a33
saranf <qorwlsdk1995@naver.com>
2021-08-03 23:32:01 +0900

    Charater 내용 추가 + Boolean Test

---
+++ BooleanTest.java
@@ -0,0 +1,10 @@
+public class BooleanTest {
+
+    public static void main(String[] args) {
+        
+        boolean isMarried = true; //참과 거짓만 표현하므로 1byte만 사용한다.
+        System.out.println(isMarried);
+
+    }
+    
+}
--- CharacterTest.java
+++ CharacterTest.java
@@ -17,6 +17,14 @@ public class CharacterTest {
         char ch='\uD55C';
         System.out.println(ch);
 
+        //character set : 문자를 숫자로 변환한 값의 셋트
+        //encodeing : 문자 -> 숫자로 변환되는것 
+        //decoding : 숫자 -> 문자로 변환하는것 
+        //Ascii code : 알파벨과 숫자 특수문자들을 1바이트에 표현하는데 사용하는 문자세트
+        //UNICODE : 전 세계 표준으로 만든 문자 세트 
+        //UTF-8 : 1바이트에서 4바이트까지 다양하게 문자를 표현할수 있음 
+        //UTF-16 : 2 바이트의 문자로 표현 
+
     }
     
 }
