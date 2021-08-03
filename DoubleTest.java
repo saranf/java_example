b0ae9648f27e1574ce24730b12d7c5fa22f64444
saranf <qorwlsdk1995@naver.com>
2021-08-03 23:11:28 +0900

    DoubleTest

---
+++ DoubleTest.java
@@ -0,0 +1,11 @@
+public class DoubleTest {
+
+    public static void main(String[] args) {
+        double dnum=3.14;
+        //float  fnum=3.14; 이대로 그대로 쓰면 double형으로 저장이 되어서 이렇게 쓰면 8byte로 잡히게 된다. 
+        float fnum=3.14f;
+        System.out.println(dnum);
+        System.out.println(fnum);
+    }
+    
+}
