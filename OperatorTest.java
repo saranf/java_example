485a141701e9d266a363751831045975c595aa6b
saranf <qorwlsdk1995@naver.com>
2021-08-05 22:32:00 +0900

    증감연산자

---
+++ OperatorTest.java
@@ -0,0 +1,10 @@
+public class OperatorTest {
+    public static void main(String[] args) {
+        int gameScore = 150;
+
+        int lastScore = ++gameScore; //gamescort +=1; 
+
+        System.out.println(lastScore);
+        System.out.println(gameScore);
+    }
+}
