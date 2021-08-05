aeae129d2006d92a88c9e739e277568658a209c2
saranf <qorwlsdk1995@naver.com>
2021-08-05 22:37:26 +0900

    관계 연산자

---
+++ RealtionalTest.java
@@ -0,0 +1,13 @@
+public class RealionalTest {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3
+
+        boolean value = (num1 > num2);
+        System.out.println(num1<num2);
+        System.out.println(num1>num2);
+        System.out.println(num1<=num2);
+        System.out.println(num1!=num2);
+    }
+}
