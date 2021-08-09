60f10644edbcdc4b78a645b0dad5780c4c6b5464
saranf <qorwlsdk1995@naver.com>
2021-08-09 09:05:02 +0900

    dowhile test

---
+++ DowhileTest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DowhileTest {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int input;
+        int sum=0; 
+
+        input= scanner.nextInt();
+
+        do{
+            sum += input;
+            input=scanner.nextInt();
+        }
+        while (input!=0);
+        System.out.println(sum);
+    }
+    
+    
+}
