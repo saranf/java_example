aebbe4477d2bbbea176eeae5dc22de95f976cc48
saranf <qorwlsdk1995@naver.com>
2021-08-10 22:43:31 +0900

    class 추상화 실습

--- java_ch01/ch02/Student.java
+++ java_ch01/ch02/Student.java
@@ -2,4 +2,10 @@ package ch02;
 
 public class Student {
     
+    int studentNumber; //변수 이름은 소문자
+    String studentName;//class 이름은 대문자
+    int majorCode;
+    String majorName;
+    int grade;
+    
 }
---
+++ java_ch01/ch02/UserInfo.java
@@ -0,0 +1,19 @@
+package ch02;
+
+public class UserInfo {
+
+    String userId;
+    String userPassword;
+    String userName;
+    String address;
+    int phoneNumber;
+    
+}
+
+//객체를 정의하고
+//각 객체의 속성을 멤버 변수로 역할을 메서드로 구현 하고
+//각 객체간의 협력을 구현 합니다.
+
+//클래스는 대문자로 시작하는것이 좋은
+//java파일 하나에 클래스는 여러 개가 있을수가 있지만 public 클래스는 하나이고 public클래스와 java파일의 이름은 동일한
+//camel notation 방식으로 운영
