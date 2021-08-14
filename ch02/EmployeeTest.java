51664975617f3a876a20d800c916ed35b70d1ff1
saranf <qorwlsdk1995@naver.com>
2021-08-14 22:38:29 +0900

    스태틱 변수

--- ch02/EmployeeTest.java
+++ ch02/EmployeeTest.java
@@ -8,7 +8,9 @@ public class EmployeeTest {
 
         employee.setEmployeeName("이순신");
 
-        System.out.println(employee.serialNum);
+        System.out.println(employee.serialNum);//class 변수로 인스턴스의 생성과는 상관없이 사용할수 있다.
+        //이미 메모리에 올라가 있기 때문이다. 
+        //클래스 이름으로 참조해서 쓰라고 해서 노란줄로 쳐져 있다.
 
         Employee employeeKim = new Employee();
         employeeKim.setEmployeeName("김유신");
