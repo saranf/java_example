343fffc4777fdf1605533349c81cd999b992c503
saranf <qorwlsdk1995@naver.com>
2021-08-14 22:31:16 +0900

    static 변수 연습

---
+++ ch02/EmployeeTest.java
@@ -0,0 +1,21 @@
+package ch02;
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        
+        Employee employee = new Employee();
+
+        employee.setEmployeeName("이순신");
+
+        System.out.println(employee.serialNum);
+
+        Employee employeeKim = new Employee();
+        employeeKim.setEmployeeName("김유신");
+        employeeKim.serialNum++;
+
+        System.out.println(employeeKim.serialNum);        
+        System.out.println(employee.serialNum);
+    }
+    
+}
