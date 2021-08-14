a7b6b69f18596c84b5d782f5487fe95610b94b31
saranf <qorwlsdk1995@naver.com>
2021-08-14 22:45:35 +0900

    static 메소드의 구현과 활용, 변수의 유효 범위

--- ch02/Employee.java
+++ ch02/Employee.java
@@ -10,6 +10,15 @@ public class Employee {
 
     //객체가 생성될때 초기화 작업은 생성자에서 해야 한다.
 
+    public static int getSerialNum() {
+        int i = 0;  //지역변수라서 x
+
+       //employName = "Lee"; // 인스턴스가 생성되지 않은 상태에서 인스턴스 변수를 사용할 수 없다.
+
+       
+        return serialNum;
+    }//중효한 변수이기 때문에 읽기만 해야 해서 getter만 사용 가능한다.
+
     public Employee(){
         serialNum ++;
         employeeId = serialNum; //기준값
--- ch02/EmployeeTest.java
+++ ch02/EmployeeTest.java
@@ -8,9 +8,11 @@ public class EmployeeTest {
 
         employee.setEmployeeName("이순신");
 
-        System.out.println(employee.serialNum);//class 변수로 인스턴스의 생성과는 상관없이 사용할수 있다.
+        System.out.println(employee.getSerialNum());//class 변수로 인스턴스의 생성과는 상관없이 사용할수 있다.
         //이미 메모리에 올라가 있기 때문이다. 
         //클래스 이름으로 참조해서 쓰라고 해서 노란줄로 쳐져 있다.
+        //인스턴스 생성과는 상관 없어서 getter안에 인스턴스 변수는 사용할수 없다.
+        //프로그램이 메모리에 있는 동안 그 영역을 차지하므로 큰 메모리를 할당하는것은 좋지 않음
 
         Employee employeeKim = new Employee();
         employeeKim.setEmployeeName("김유신");
